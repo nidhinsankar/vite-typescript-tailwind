71f7f8a3adc0b63315dfd295f8fe7966c5ddf21d
nidhinsankar <nidhinsankar1@gmail.com>
2024-03-07 12:15:57 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="bg-red-400 font-bold text-center">
+        <p>
+          A react template repository with tailwindcss, Typescript scaffolded by
+          vite
+        </p>
+        <h2>Feel free to start building your project</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
